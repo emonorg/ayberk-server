3bbcb59ff9884634c69e84a8931217dd37fe8e97
Emad <emonoorg@gmail.com>
2022-09-26 21:53:36 +0200

    Change error of request without authorization token

--- src/auth/middlewares/authentication.middleware.ts
+++ src/auth/middlewares/authentication.middleware.ts
@@ -1,10 +1,5 @@
-import {
-  Injectable,
-  NestMiddleware,
-  UnauthorizedException,
-} from '@nestjs/common';
-import { NextFunction, Request, Response } from 'express';
-import { request } from 'http';
+import { ForbiddenException, Injectable, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Response } from 'express';
 import { AuthenticatedRequest } from 'src/lib/interfaces/authenticatedRequest.interface';
 import { Operator } from 'src/operator/models/operator.model';
 import { AuthService } from '../auth.service';
@@ -21,11 +16,11 @@ export class AuthenticationMiddleware implements NestMiddleware {
       const accessToken = req.headers.authorization.split(' ')[1];
       const operatorSession =
         await this.authService.validateSessionAndGetOperator(accessToken);
-      if (!operatorSession) throw new UnauthorizedException();
+      if (!operatorSession) throw new ForbiddenException();
       req.principle = operatorSession.operator;
       next();
     } catch (e) {
-      if (e instanceof TypeError) throw new UnauthorizedException();
+      if (e instanceof TypeError) throw new ForbiddenException();
     }
   }
 }
